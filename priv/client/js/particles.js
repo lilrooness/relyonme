d668aafd4d78eb55bd48a4cc0e5a58b6fcaf39ac
Joe Frangoudes <jfrangoudes@gmail.com>
2017-12-16 15:34:51 +0000

    add particle rendering

--- priv/client/js/particles.js
+++ priv/client/js/particles.js
@@ -1,10 +1,19 @@
 var Particle = function(x, y, ttl) {
 	this.entity = new Entity(x, y);
+	this.colour = {
+		"r": 255,
+		"g": 255,
+		"b": 255
+	}; 
 }
 
 Particle.prototype.update = function() {
 	this.entity.updatex();
 	this.entity.updatey();
+}
+
+Particle.prototype.render = function(context) {
+	context.fillRect(this.x, this.y, 10, 10);
 }
 
 var Explosion = function(x, y, size) {
@@ -17,5 +26,11 @@ var Explosion = function(x, y, size) {
 			y: (Math.random() * 2) - 1
 		})
 		this.particles.push()
+	}
+}
+
+Explosion.prototype.render = function(context) {
+	for(var i=0; i<particles.length; i++) {
+		particles[i].render(context);
 	}
 }
